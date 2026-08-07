2225185c658affd5aeab0581f23694488a7d0ace
Yorjas4 <yor_jas@hotmail.com>
2026-08-07 16:40:13 -0600

    cambio y traza de bienes a usuarios y tecnicos

--- backend/src/bienes/bienes.module.ts
+++ backend/src/bienes/bienes.module.ts
@@ -1,15 +1,11 @@
 import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
-import { BienMueble, ServidorBien, SUsuario, Usuario } from '../database/models';
+import { SUsuario, Usuario } from '../database/models';
 import { BienesController } from './bienes.controller';
 import { BienesService } from './bienes.service';
 
 @Module({
-  imports: [
-    SequelizeModule.forFeature([Usuario]),
-    SequelizeModule.forFeature([SUsuario], 'saf'),
-    SequelizeModule.forFeature([ServidorBien, BienMueble], 'bienes'),
-  ],
+  imports: [SequelizeModule.forFeature([Usuario]), SequelizeModule.forFeature([SUsuario], 'saf')],
   controllers: [BienesController],
   providers: [BienesService],
   exports: [BienesService],
